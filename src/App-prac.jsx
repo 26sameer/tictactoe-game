d6826c13f00434d5c7319ccc0d00ae783dbc1df7
Sameerbasha <sameer26basha@gmail.com>
2023-04-03 11:17:54 +0530

    Adding input functionality from a single player

---
+++ src/App-prac.jsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react';
+// import './styles.scss';
+
+// function App() {
+//   const [count, setCount] = useState(() => {
+//     console.log('FUnc run');
+//     return 0;
+//   });
+
+//   function decrementCount() {
+//     setCount(prevCount => prevCount - 1);
+//   }
+
+//   function incrementCount() {
+//     setCount(prevCount => prevCount + 1);
+//   }
+
+//   return (
+//     <div className="app">
+//       <button onClick={decrementCount}>-</button>
+//       <p>{count}</p>
+//       <button onClick={incrementCount}>+</button>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,47 @@
+import { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const squareClick = clickedPosition => {
+    setSquares(currentSquare => {
+      return currentSquare.map((squareValue, pos) => {
+        if (clickedPosition === pos) {
+          return 'X';
+        }
+        return squareValue;
+      });
+    });
+  };
+
+  const renderSquare = position => {
+    return (
+      <Square
+        value={squares[position]}
+        onClick={() => squareClick(position)}
+      ></Square>
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
 };
-
 export default Board;
